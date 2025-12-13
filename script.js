126197c3ff831e403a4e309d2879f9f10129557c
Adolf Nohara <adolfnohara@gmail.com>
2025-12-13 20:40:09 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function sendMessage() {
+  const input = document.getElementById("user-input");
+  const message = input.value.trim();
+  if (message === "") return;
+
+  addMessage(message, "user");
+  input.value = "";
+
+  setTimeout(() => {
+    const reply = getBotReply(message);
+    addMessage(reply, "bot");
+  }, 500);
+}
+
+function addMessage(text, sender) {
+  const chatBox = document.getElementById("chat-box");
+  const msg = document.createElement("div");
+  msg.className = `msg ${sender}`;
+  msg.innerText = text;
+  chatBox.appendChild(msg);
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+function getBotReply(msg) {
+  msg = msg.toLowerCase();
+
+  if (msg.includes("hello") || msg.includes("hi"))
+    return "Hello! 😊 How can I help you?";
+
+  if (msg.includes("html"))
+    return "HTML is used to create the structure of web pages.";
+
+  if (msg.includes("css"))
+    return "CSS is used to style websites.";
+
+  if (msg.includes("javascript"))
+    return "JavaScript makes websites interactive.";
+
+  if (msg.includes("your name"))
+    return "I'm your beginner AI Assistant 🤖";
+
+  return "Sorry, I don't understand yet. I'm still learning 🙂";
+}
